763d324337a693d5dbf46cb0ca1626ec5aa365af
Øystein Grøvlen <oystein.grovlen@gmail.com>
2022-01-16 15:07:14 +0100

    Added option for number of tables

--- sysbench_basic.sh
+++ sysbench_basic.sh
@@ -3,14 +3,15 @@
 MAXTHREADS=4096
 TIME=300
 HOST=$1
-SIZE=$2
-TESTCONFIG=$3
+TABLES=$2
+ROWS=$3
+TESTCONFIG=$4
 
 # Run short test
 echo $TESTCONFIG
 for TEST in point_select read_only update_non_index read_write
 do
   echo $TEST  
-  sysbench_oltp.sh $TEST $HOST $TIME $SIZE $MAXTHREADS $TESTCONFIG
+  sysbench_oltp.sh $TEST $HOST $TIME $TABLES $ROWS $MAXTHREADS $TESTCONFIG
 done
 echo "END TEST"
--- sysbench_oltp.sh
+++ sysbench_oltp.sh
@@ -3,16 +3,17 @@
 TEST=$1
 HOST=$2
 TIME=$3
-SIZE=$4
-MAXTHREADS=$5
-TESTCONFIG=$6
+TABLES=$4
+ROWS=$5
+MAXTHREADS=$6
+TESTCONFIG=$7
 TESTDIR=${HOME}/test/$6_$1
 
 mkdir $TESTDIR
 i=1
 while [ $i -le $MAXTHREADS ]
 do
-  sysbench --threads=$i --time=$TIME --warmup-time=30 --report-interval=10 --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=mysql --mysql-ssl=REQUIRED --tables=8  --table-size=$SIZE sysbench/src/lua/oltp_${TEST}.lua run > $TESTDIR/sysbench_$i.out
+  sysbench --threads=$i --time=$TIME --warmup-time=30 --report-interval=10 --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=mysql --mysql-ssl=REQUIRED --tables=$TABLES  --table-size=$ROWS sysbench/src/lua/oltp_${TEST}.lua run > $TESTDIR/sysbench_$i.out
   awk -F\( -v i=$i '/transactions:/ {print i, $2}' $TESTDIR/sysbench_$i.out | awk '{print $1, $2}'
   i=$(( 2*$i ))
 done
