b53f1530df1dbb48e81443953d23ee5ca3bbb55a
Øystein Grøvlen <oystein.grovlen@gmail.com>
2022-01-17 09:20:04 +0100

    Configurable number of tables

--- sysbench_one.sh
+++ sysbench_one.sh
@@ -3,11 +3,12 @@
 TEST=$1
 HOST=$2
 TIME=$3
-ROWS=$4
-THREADS=$5
-TESTCONFIG=$6
-TESTDIR=${HOME}/test/$6_$1
+TABLES=$4
+ROWS=$5
+THREADS=$6
+TESTCONFIG=$7
+TESTDIR=${HOME}/test/$7_$1
 
 mkdir $TESTDIR
-sysbench --threads=$THREADS --time=$TIME --warmup-time=30 --report-interval=10 --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=mysql --mysql-ssl=REQUIRED --tables=8  --table-size=$ROWS sysbench/src/lua/oltp_${TEST}.lua run > $TESTDIR/sysbench_$THREADS.out
+sysbench --threads=$THREADS --time=$TIME --warmup-time=30 --report-interval=10 --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=mysql --mysql-ssl=REQUIRED --tables=$TABLES  --table-size=$ROWS sysbench/src/lua/oltp_${TEST}.lua run > $TESTDIR/sysbench_$THREADS.out
 
