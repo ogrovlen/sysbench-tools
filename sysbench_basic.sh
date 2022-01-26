333463eb5afa5163f88b462a2c542cca87a19ea9
Øystein Grøvlen <oystein.grovlen@gmail.com>
2022-01-26 18:56:29 +0100

    More tracing

--- sysbench_basic.sh
+++ sysbench_basic.sh
@@ -7,11 +7,13 @@ TABLES=$2
 ROWS=$3
 TESTCONFIG=$4
 
-# Run short test
 echo $TESTCONFIG
+# Warmup
+sysbench --threads=128 --time=300 --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=mysql --mysql-ssl=REQUIRED --tables=$TABLES  --table-size=$ROWS sysbench/src/lua/oltp_point_select.lua run > /dev/null
+
 for TEST in point_select read_only update_non_index read_write
 do
   echo $TEST  
-  sysbench_oltp.sh $TEST $HOST $TIME $TABLES $ROWS $MAXTHREADS $TESTCONFIG
+  sysbench_oltp.sh $TEST $HOST $TIME $TABLES $ROWS $MAXTHREADS $TESTCONFIG 
 done
 echo "END TEST"
--- sysbench_oltp.sh
+++ sysbench_oltp.sh
@@ -13,7 +13,8 @@ mkdir $TESTDIR
 i=1
 while [ $i -le $MAXTHREADS ]
 do
-  sysbench --threads=$i --time=$TIME --warmup-time=30 --report-interval=10 --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=mysql --mysql-ssl=REQUIRED --tables=$TABLES  --table-size=$ROWS --db-ps-mode=disable sysbench/src/lua/oltp_${TEST}.lua run > $TESTDIR/sysbench_$i.out
-  awk -F\( -v i=$i '/transactions:/ {print i, $2}' $TESTDIR/sysbench_$i.out | awk '{print $1, $2}'
+  echo "sysbench --threads=$i --time=$TIME --warmup-time=30 --report-interval=10 --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=mysql --mysql-ssl=REQUIRED --tables=$TABLES  --table-size=$ROWS --db-ps-mode=disable sysbench/src/lua/oltp_${TEST}.lua run" > $TESTDIR/sysbench_$i.out
+  sysbench --threads=$i --time=$TIME --warmup-time=30 --report-interval=10 --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=mysql --mysql-ssl=REQUIRED --tables=$TABLES  --table-size=$ROWS --db-ps-mode=disable sysbench/src/lua/oltp_${TEST}.lua run >> $TESTDIR/sysbench_$i.out
+  awk -F\( -v i=$i '/transactions:/ {print i, $2}' $TESTDIR/sysbench_$i.out | awk '{print $1, $2}' | tee -a $TESTDIR/summary.out
   i=$(( 2*$i ))
 done
