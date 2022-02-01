8809f5ebd19799a34fc96912075c09889fee2cb2
Øystein Grøvlen <oystein.grovlen@gmail.com>
2022-02-01 20:14:45 +0100

    Fix scale typo

--- sysbench_tpcc_all.sh
+++ sysbench_tpcc_all.sh
@@ -11,7 +11,7 @@ then
    ssh -i .ssh/oysteing-ssh.pem $HOST cat /etc/sysconfig/mysql
 fi
 
-for SCALE in 100 1000 1000
+for SCALE in 100 1000 10000
 do
   echo $TEST  
   sysbench_tpcc.sh $HOST $SCALE $TIME $MAXTHREADS $TESTCONFIG 
