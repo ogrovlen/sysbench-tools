e40552d7236103b3ac1aa8e31bae8d83a9d84cf7
Øystein Grøvlen <oystein.grovlen@gmail.com>
2022-03-15 09:32:35 +0100

    ...

--- sysbench_oltp.sh
+++ sysbench_oltp.sh
@@ -10,12 +10,12 @@ TESTCONFIG=$7
 TESTDIR=${HOME}/test/${TESTCONFIG}_$TEST
 
 mkdir $TESTDIR
-mysql --host=$HOST -umysql -pMySQL#123 -e 'select * from performance_schema.global_variables' > $TESTDIR/global.cnf
+mysql --host=$HOST -umysql -pMySQL\#123 -e 'select * from performance_schema.global_variables' > $TESTDIR/global.cnf
 i=1
 while [ $i -le $MAXTHREADS ]
 do
-  echo "sysbench --threads=$i --time=$TIME --warmup-time=30 --report-interval=10 --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=MySQL#123 --mysql-ssl=REQUIRED --tables=$TABLES  --table-size=$ROWS --db-ps-mode=disable sysbench/src/lua/oltp_${TEST}.lua run" > $TESTDIR/sysbench_$i.out
-  sysbench --threads=$i --time=$TIME --warmup-time=30 --report-interval=10 --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=MySQL#123 --mysql-ssl=REQUIRED --tables=$TABLES  --table-size=$ROWS --db-ps-mode=disable sysbench/src/lua/oltp_${TEST}.lua run >> $TESTDIR/sysbench_$i.out
+  echo "sysbench --threads=$i --time=$TIME --warmup-time=30 --report-interval=10 --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=MySQL\#123 --mysql-ssl=REQUIRED --tables=$TABLES  --table-size=$ROWS --db-ps-mode=disable sysbench/src/lua/oltp_${TEST}.lua run" > $TESTDIR/sysbench_$i.out
+  sysbench --threads=$i --time=$TIME --warmup-time=30 --report-interval=10 --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=MySQL\#123 --mysql-ssl=REQUIRED --tables=$TABLES  --table-size=$ROWS --db-ps-mode=disable sysbench/src/lua/oltp_${TEST}.lua run >> $TESTDIR/sysbench_$i.out
   awk -F\( -v i=$i '/transactions:/ {print i, $2}' $TESTDIR/sysbench_$i.out | awk '{print $1, $2}' | tee -a $TESTDIR/summary.out
   i=$(( 2*$i ))
 done
