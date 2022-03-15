465fb4f20d714117adf528a31bc944f5d8534ba9
Øystein Grøvlen <oystein.grovlen@gmail.com>
2022-03-15 08:43:25 +0100

    Force NULL for TPCC. Record config variables

--- sysbench_tpcc.sh
+++ sysbench_tpcc.sh
@@ -10,13 +10,14 @@ TESTDIR=${HOME}/test/${TESTCONFIG}_${DB}
 
 mkdir $TESTDIR
 echo $TESTDIR
+mysql --host=$HOST -umysql -pMySQL#123 -e 'select * from performance_schema.global_variables' > $TESTDIR/global.cnf
 # Warmup
-sysbench /BMK/sb_exec/lua/TPCC-dim.lua --db-driver=mysql --threads=128 --scale=$SCALE --time=$TIME --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=mysql --mysql-ssl=REQUIRED --mysql-db=$DB --tables=1 --events=0 --thread-init-timeout=0 --rate=0 run > /dev/null
+sysbench /BMK/sb_exec/lua/TPCC-dim.lua --db-driver=mysql --threads=128 --scale=$SCALE --time=$TIME --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=mysql --mysql-ssl=REQUIRED --mysql-db=$DB --tables=1 --events=0 --thread-init-timeout=0 --rate=0 --force_null=1 run > /dev/null
 i=1
 while [ $i -le $MAXTHREADS ]
 do
-  echo "sysbench /BMK/sb_exec/lua/TPCC-dim.lua --db-driver=mysql --threads=$i --scale=$SCALE --time=$TIME --warmup-time=30 --report-interval=10 --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=mysql --mysql-ssl=REQUIRED --mysql-db=$DB --tables=1 --events=0 --thread-init-timeout=0 --rate=0 run" > $TESTDIR/tpcc_$i.out
-  sysbench /BMK/sb_exec/lua/TPCC-dim.lua --db-driver=mysql --threads=$i --scale=$SCALE --time=$TIME --warmup-time=30 --report-interval=10 --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=mysql --mysql-ssl=REQUIRED --mysql-db=$DB --tables=1 --events=0 --thread-init-timeout=0 --rate=0 run >> $TESTDIR/tpcc_$i.out
+  echo "sysbench /BMK/sb_exec/lua/TPCC-dim.lua --db-driver=mysql --threads=$i --scale=$SCALE --time=$TIME --warmup-time=30 --report-interval=10 --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=mysql --mysql-ssl=REQUIRED --mysql-db=$DB --tables=1 --events=0 --thread-init-timeout=0 --rate=0 --force_null=1 run" > $TESTDIR/tpcc_$i.out
+  sysbench /BMK/sb_exec/lua/TPCC-dim.lua --db-driver=mysql --threads=$i --scale=$SCALE --time=$TIME --warmup-time=30 --report-interval=10 --rand-type=uniform --mysql-host=$HOST --mysql-user=mysql --mysql-password=mysql --mysql-ssl=REQUIRED --mysql-db=$DB --tables=1 --events=0 --thread-init-timeout=0 --rate=0 --force_null=1 run >> $TESTDIR/tpcc_$i.out
   awk -F\( -v i=$i '/transactions:/ {print i, $2}' $TESTDIR/tpcc_$i.out | awk '{print $1, $2}' | tee -a $TESTDIR/summary.out
   i=$(( 2*$i ))
 done
